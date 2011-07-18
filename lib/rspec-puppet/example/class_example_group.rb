935538be2ac2247545e9e4a95716d9bba8bf42ea
Dan Bode <dan@puppetlabs.com>
2011-07-17 22:12:31 -0700

    Added support for param classes
    Added support for param classes in the class
    example group.
    
    This commit also changes the call to get the
    description from:
    
    self.class.meta[:example_group][:full_description]
    
    to self.class.top_level_description.downcase
    
    This provides better support nested descriptions.
    (The previous code was concatenating the nested
    descriptions which was not creating a valid
    klass name for my working example)
    
    Now, the only requirement is that the top level
    description is the same as the class name.

--- lib/rspec-puppet/example/class_example_group.rb
+++ lib/rspec-puppet/example/class_example_group.rb
@@ -8,14 +8,18 @@ module RSpec::Puppet
 
     def catalogue
       Puppet[:modulepath] = module_path
-      Puppet[:code] = "include #{self.class.metadata[:example_group][:full_description].downcase}"
-
       unless facts = Puppet::Node::Facts.find(Puppet[:certname])
         raise "Could not find facts for #{Puppet[:certname]}"
       end
 
       unless node = Puppet::Node.find(Puppet[:certname])
         raise "Could not find node #{Puppet[:certname]}"
+      klass_name = self.class.top_level_description.downcase
+      if params || params == {}
+        Puppet[:code] = "include #{klass_name}"
+      else
+        Puppet[:code] = 'class' + " { " + klass_name + ": " + params.keys.map { |r|        "#{r.to_s} => '#{params[r].to_s}'"
+      }.join(', ') + " }"
       end
 
       node.merge(facts.values)
