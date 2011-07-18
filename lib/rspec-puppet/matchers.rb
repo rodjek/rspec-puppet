7ba60da794a3cb73e656b77a56585861b615d2c9
Tim Sharpe <tim@github.com>
2011-07-19 07:17:18 +1000

    Abuse method_missing to create a generic matcher for resourcs

--- lib/rspec-puppet/matchers.rb
+++ lib/rspec-puppet/matchers.rb
@@ -1,3 +1,3 @@
-require 'rspec-puppet/matchers/create_exec'
+require 'rspec-puppet/matchers/create_generic'
 require 'rspec-puppet/matchers/create_resource'
 require 'rspec-puppet/matchers/include_class'
---
+++ lib/rspec-puppet/matchers/create_generic.rb
@@ -0,0 +1,74 @@
+module RSpec::Puppet
+  module Matchers
+    class CreateGeneric
+      def initialize(*args, &block)
+        @exp_resource_type = args.shift.to_s.gsub(/^create_/, '')
+        @args = args
+        @block = block
+        @referenced_type = referenced_type(@exp_resource_type)
+        @title = args[0]
+      end
+
+      def method_missing(method, *args, &block)
+        if method.to_s =~ /^with_/
+          param = method.to_s.gsub(/^with_/, '')
+          (@expected_params ||= []) << [param, args[0]]
+          self
+        else
+          super
+        end
+      end
+
+      def matches?(catalogue)
+        ret = true
+        resources = catalogue.resources.select { |r|
+          r.type == @referenced_type
+        }.select { |r|
+          r.title == @title if r.respond_to? :title
+        }
+
+        unless resources.length == 1
+          ret = false
+        else
+          if @expected_params
+            @expected_params.each do |name, value|
+              unless resources.first.send(:parameters)[name.to_sym].to_s == value.to_s
+                ret = false
+                (@errors ||= []) << "#{name.to_s} set to `#{value.inspect}`"
+              end
+            end
+          end
+        end
+
+        ret
+      end
+
+      def failure_message_for_should
+        "expected that the catalogue would contain #{@referenced_type}[\"#{@title}\"]#{errors}"
+      end
+
+      def failure_message_for_should_not
+        "expected that the catalogue would not contain #{@referenced_type}[\"#{@title}\"]#{errors}"
+      end
+
+      def description
+        "create #{@referenced_type}[\"#{@title}\"]"
+      end
+
+    private
+
+      def referenced_type(type)
+        type.split('-').map { |r| r.capitalize }.join('::')
+      end
+
+      def errors
+        @errors.nil? ? "" : " with #{@errors.join(', ')}"
+      end
+    end
+
+    def method_missing(method, *args, &block)
+      return RSpec::Puppet::Matchers::CreateGeneric.new(method, *args, &block) if method.to_s =~ /^create_/
+      super
+    end
+  end
+end
