effb1facc993aee121906f98e7fefebc586bc0a1
Tim Sharpe <tim@github.com>
2011-07-18 16:02:17 +1000

    Eugh, there was a lot of stupid in there

--- lib/rspec-puppet/example/define_example_group.rb
+++ lib/rspec-puppet/example/define_example_group.rb
@@ -8,20 +8,18 @@ module RSpec::Puppet
 
     def catalogue
       Puppet[:modulepath] = module_path
-      Puppet[:code] = self.class.metadata[:example_group][:full_description].downcase + " { " + name + ": " + params.keys.map { |r|
+      Puppet[:code] = self.class.metadata[:example_group][:full_description].downcase + " { \"" + name + "\": " + params.keys.map { |r|
         "#{r.to_s} => '#{params[r].to_s}'"
       }.join(', ') + " }"
 
       nodename = self.respond_to?(:node) ? node : Puppet[:certname]
-      facts_val = {}
-      facts_val.merge(facts) if self.respond_to? :facts
+      facts_val = self.respond_to?(:facts) ? facts : {}
 
-      node = Puppet::Node.new(nodename)
-      facts = Puppet::Node::Facts.new(nodename, facts_val)
+      node_obj = Puppet::Node.new(nodename)
 
-      node.merge(facts.values)
+      node_obj.merge(facts_val)
 
-      Puppet::Resource::Catalog.find(node.name, :use_node => node)
+      Puppet::Resource::Catalog.find(node_obj.name, :use_node => node_obj)
     end
   end
 end
