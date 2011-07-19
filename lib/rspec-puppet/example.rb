ae27b394a64a16b011f64d0fd3734a14549178b8
Tim Sharpe <tim@github.com>
2011-07-19 12:19:09 +1000

    Plurals are good

--- lib/rspec-puppet/example.rb
+++ lib/rspec-puppet/example.rb
@@ -7,10 +7,10 @@ RSpec::configure do |c|
   end
 
   c.include RSpec::Puppet::DefineExampleGroup, :type => :define, :example_group => {
-    :file_path => c.escaped_path(%w[spec define])
+    :file_path => c.escaped_path(%w[spec defines])
   }
 
   c.include RSpec::Puppet::ClassExampleGroup, :type => :class, :example_group => {
-    :file_path => c.escaped_path(%w[spec class])
+    :file_path => c.escaped_path(%w[spec classes])
   }
 end
