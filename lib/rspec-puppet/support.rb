ac4bc432d0b5f815bfbdf8ac118f9e2f3b40b834
Tim Sharpe <tim@sharpe.id.au>
2011-08-04 18:52:48 -0700

    It works

--- lib/rspec-puppet/support.rb
+++ lib/rspec-puppet/support.rb
@@ -1,9 +1,47 @@
+class Puppet::Parser::Compiler
+  def compile
+    set_node_parameters
+    create_settings_scope
+
+    evaluate_main
+
+    evaluate_ast_node
+
+    mock_virtual_resources.each do |r|
+      add_resource(topscope, r)
+    end
+
+
+    evaluate_node_classes
+
+    evaluate_generators
+
+    finish
+
+    fail_on_unevaluated
+
+    @catalog
+  end
+end
+
 module RSpec::Puppet
   module Support
     def build_catalog(nodename, facts_val, additional_resources)
       node_obj = Puppet::Node.new(nodename)
 
       node_obj.merge(facts_val)
+
+      additional_resources = [additional_resources] if additional_resources.is_a? Hash
+      scope = Puppet::Parser::Scope.new
+      additional_resources = additional_resources.map do |r|
+        res = Puppet::Parser::Resource.new(r[:type], r[:title], {:virtual => true, :scope => scope})
+        r[:parameters].keys.each { |k|
+        res[k] = r[:parameters][k]
+        }
+        res
+      end
+
+      Puppet::Parser::Compiler.any_instance.stub(:mock_virtual_resources).and_return(additional_resources)
 
       # trying to be compatible with 2.7 as well as 2.6
       if Puppet::Resource::Catalog.respond_to? :find
@@ -13,13 +51,7 @@ module RSpec::Puppet
         catalogue = Puppet::Face[:catalog, :current].find(node_obj.name, :use_node => node_obj)
       end
 
-      additional_resources = [additional_resources] if additional_resources.is_a? Hash
-      additional_resources.each do |resource|
-        catalogue.add_resource Puppet::Resource.new(resource[:type], resource[:title], {:virtual => true, :parameters => resource[:parameters]})
-      end
-
-      p catalogue.resource('File[foo]').virtual?
-      catalogue.compile
+      catalogue
     end
   end
 end
