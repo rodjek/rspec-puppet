37906c4c6bd4dd853144514f6ffabe494f51b009
Tim Sharpe <tim@sharpe.id.au>
2011-09-17 08:16:08 +1000

    default manifest_dir, manifest & template_dir to nil

--- lib/rspec-puppet.rb
+++ lib/rspec-puppet.rb
@@ -4,7 +4,7 @@ require 'rspec-puppet/example'
 
 RSpec.configure do |c|
   c.add_setting :module_path, :default => '/etc/puppet/modules'
-  c.add_setting :manifest_dir, :default => '/etc/puppet/manifests'
-  c.add_setting :manifest, :default => '/etc/puppet/manifests/site.pp'
-  c.add_setting :template_dir, :default => '/etc/puppet/templates'
+  c.add_setting :manifest_dir, :default => nil
+  c.add_setting :manifest, :default => nil
+  c.add_setting :template_dir, :default => nil
 end
