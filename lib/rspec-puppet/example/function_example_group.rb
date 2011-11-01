7f7e74f02c2c3a0047643f7fb86dec9ba542da63
Tim Sharpe <tim@sharpe.id.au>
2011-11-01 21:57:42 +1300

    Munge libdir to autoload module plugins without pluginsync

--- lib/rspec-puppet/example/function_example_group.rb
+++ lib/rspec-puppet/example/function_example_group.rb
@@ -5,6 +5,8 @@ module RSpec::Puppet
     def subject
       function_name = self.class.top_level_description.downcase
 
+      Puppet[:modulepath] = self.respond_to?(:module_path) ? module_path : RSpec.configuration.module_path
+      Puppet[:libdir] = Dir["#{Puppet[:modulepath]}/*/lib"].entries.join(':')
       Puppet::Parser::Functions.autoloader.loadall
 
       scope = Puppet::Parser::Scope.new
