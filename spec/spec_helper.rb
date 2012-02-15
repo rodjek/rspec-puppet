a3fff4516451e9cc6f8fb8d8d372e1338a6bc9d9
Tim Sharpe <tim@sharpe.id.au>
2012-02-15 16:27:46 +1100

    Organise fixtures into modules/ & manifests/

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 require 'rspec-puppet'
 
 RSpec.configure do |c|
-  c.module_path = File.join(File.dirname(File.expand_path(__FILE__)), 'fixtures')
+  c.module_path = File.join(File.dirname(File.expand_path(__FILE__)), 'fixtures', 'modules')
+  c.manifest_dir = File.join(File.dirname(File.expand_path(__FILE__)), 'fixtures', 'manifests')
 end
