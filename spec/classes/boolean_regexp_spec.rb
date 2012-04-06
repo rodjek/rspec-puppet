1ef8c26388def4753c13bcdd01ab7083669e4a3c
Tim Sharpe <tim@sharpe.id.au>
2012-04-06 15:05:49 -0700

    Remove a couple of parameterised class tests for 0.25.x

--- spec/classes/boolean_regexp_spec.rb
+++ spec/classes/boolean_regexp_spec.rb
@@ -1,10 +1,12 @@
 require 'spec_helper'
 
-describe 'boolean' do
-  let(:title) { 'bool.testing' }
-  let(:params) { { :bool => false } }
-  let(:message_re) { /bool is false/ }
+if Puppet::PUPPETVERSION !~ /^0\.2/
+  describe 'boolean' do
+    let(:title) { 'bool.testing' }
+    let(:params) { { :bool => false } }
+    let(:message_re) { /bool is false/ }
 
-  it { should create_notify("bool testing").with_message(message_re) }
-  it { should_not create_notify("bool testing").with_message(/true/) }
+    it { should create_notify("bool testing").with_message(message_re) }
+    it { should_not create_notify("bool testing").with_message(/true/) }
+  end
 end
--- spec/classes/boolean_spec.rb
+++ spec/classes/boolean_spec.rb
@@ -1,10 +1,11 @@
 require 'spec_helper'
 
-describe 'boolean' do
-  let(:title) { 'bool.testing' }
-  let(:params) { { :bool => false } }
+if Puppet::PUPPETVERSION !~ /0\.2/
+  describe 'boolean' do
+    let(:title) { 'bool.testing' }
+    let(:params) { { :bool => false } }
   
-  it { should create_notify("bool testing")\
-    .with_message("This will print when \$bool is false.") }
-
+    it { should create_notify("bool testing")\
+      .with_message("This will print when \$bool is false.") }
+  end
 end
