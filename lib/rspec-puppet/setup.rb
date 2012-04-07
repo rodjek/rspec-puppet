1034302b7c235629c6a697dc4074976e2c7f5ac6
Tim Sharpe <tim@sharpe.id.au>
2012-04-07 12:53:22 -0700

    Not sure where the second .. came from

--- lib/rspec-puppet/setup.rb
+++ lib/rspec-puppet/setup.rb
@@ -87,7 +87,7 @@ module RSpec::Puppet
       content = <<-EOF
 require 'rspec-puppet'
 
-fixture_path = File.expand_path(File.join(__FILE__, '..', '..', 'fixtures'))
+fixture_path = File.expand_path(File.join(__FILE__, '..', 'fixtures'))
 
 RSpec.configure do |c|
   c.module_path = File.join(fixture_path, 'modules')
