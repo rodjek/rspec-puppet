5cdfd01248a662721b6dee7b99d4399e6003b6f3
Sven Mueller <smu@google.com>
2012-12-14 13:34:32 +0100

    Add a new matcher to check that all relationship can be fulfilled. In other words: That any resource referenced to by a require, subscribe, before or notify is defined somewhere in the catalogue.

--- lib/rspec-puppet/matchers.rb
+++ lib/rspec-puppet/matchers.rb
@@ -1,4 +1,5 @@
 require 'rspec-puppet/matchers/create_generic'
 require 'rspec-puppet/matchers/create_resource'
 require 'rspec-puppet/matchers/include_class'
+require 'rspec-puppet/matchers/include_all_deps'
 require 'rspec-puppet/matchers/run'
---
+++ spec/hosts/goddephost_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe 'gooddephost' do
+  it { should include_all_deps() }
+end
