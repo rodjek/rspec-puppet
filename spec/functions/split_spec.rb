1f3b7163461c5e85d3bce14749f2b43f4e990ead
Tim Sharpe <tim@sharpe.id.au>
2013-02-26 14:06:55 +1100

    Boom

--- spec/functions/split_spec.rb
+++ spec/functions/split_spec.rb
@@ -4,9 +4,15 @@ describe 'split' do
   it { should run.with_params('aoeu', 'o').and_return(['a', 'eu']) }
   it { should_not run.with_params('foo').and_raise_error(Puppet::DevError) }
 
-  it { should run.with_params('foo').and_raise_error(Puppet::ParseError) }
+  if Puppet.version =~ /\A3\.1/
+    expected_error = ArgumentError
+  else
+    expected_error = Puppet::ParseError
+  end
+
+  it { should run.with_params('foo').and_raise_error(expected_error) }
 
   it 'should fail with one argument' do
-    expect { subject.call(['foo']) }.to raise_error(Puppet::ParseError)
+    expect { subject.call(['foo']) }.to raise_error(expected_error)
   end
 end
