185a7e3b628f52adf3f04e0f986b4d8efbc5a8c2
Tim Sharpe <tim@sharpe.id.au>
2013-02-26 14:24:55 +1100

    Improved rake file for rspec-puppet-init
    Closes #66

--- lib/rspec-puppet/setup.rb
+++ lib/rspec-puppet/setup.rb
@@ -119,13 +119,34 @@ EOF
     end
 
     def self.safe_create_rakefile
-      content = <<-EOF
+      content = <<-'EOF'
 require 'rake'
-
 require 'rspec/core/rake_task'
 
-RSpec::Core::RakeTask.new(:spec) do |t|
-  t.pattern = 'spec/*/*_spec.rb'
+desc "Run all RSpec code examples"
+RSpec::Core::RakeTask.new(:rspec) do |t|
+  t.rspec_opts = File.read("spec/spec.opts").chomp || ""
+end
+
+SPEC_SUITES = (Dir.entries('spec') - ['.', '..','fixtures']).select {|e| File.directory? "spec/#{e}" }
+namespace :rspec do
+  SPEC_SUITES.each do |suite|
+    desc "Run #{suite} RSpec code examples"
+    RSpec::Core::RakeTask.new(suite) do |t|
+      t.pattern = "spec/#{suite}/**/*_spec.rb"
+      t.rspec_opts = File.read("spec/spec.opts").chomp || ""
+    end
+  end
+end
+task :default => :rspec
+
+begin
+  if Gem::Specification::find_by_name('puppet-lint')
+    require 'puppet-lint/tasks/puppet-lint'
+    PuppetLint.configuration.ignore_paths = ["spec/**/*.pp", "vendor/**/*.pp"]
+    task :default => [:rspec, :lint]
+  end
+rescue Gem::LoadError
 end
 EOF
       if File.exists? 'Rakefile'
