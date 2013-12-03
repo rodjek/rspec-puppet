1d0fdd5d053225e42b82fb6d9c474b68b03c362f
Tim Sharpe <tim@sharpe.id.au>
2013-12-04 10:31:48 +1100

    Deprecate include_class in favour of contain_class

--- spec/classes/test_classes_used_spec.rb
+++ spec/classes/test_classes_used_spec.rb
@@ -1,9 +1,15 @@
 require 'spec_helper'
 
 describe 'test::classes_used' do
-  it { should include_class('test::bare_class') }
-  it { should_not include_class('test::parameterised_class') }
+  it {
+    RSpec.should receive(:deprecate).with(:include_class, :replacement => :contain_class)
+    should include_class('test::bare_class')
+  }
+  it {
+    RSpec.should receive(:deprecate).with(:include_class, :replacement => :contain_class)
+    should include_class('test::parameterised_class')
+  }
 
   it { should contain_class('test::parameterised_class').with_text('bar') }
-  it { should_not contain_class('test::bare_class') }
+  it { should contain_class('test::bare_class') }
 end
--- spec/hosts/testhost_spec.rb
+++ spec/hosts/testhost_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
 
 describe 'testhost' do
-  it { should include_class('sysctl::common') }
+  it { should contain_class('sysctl::common') }
 end
