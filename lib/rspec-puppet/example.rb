59d7ceab45635043849ea6380648dbcf15a69f6d
Dan <bodepd@gmail.com>
2014-02-27 14:54:29 -0800

    Add testing support for Puppet Types
    This commit adds support for testing the following
    common features of types:
    
    * parameters available
    * properties available
    * features
    * default provider
    * that param value(s) are valid
    
    There are still a few things that need to be implemented:
    * default parameter values
    * autorequires
    * munging
    * .to_s methods (I am not sure how to test this :) )

--- lib/rspec-puppet/example.rb
+++ lib/rspec-puppet/example.rb
@@ -3,8 +3,11 @@ require 'rspec-puppet/example/define_example_group'
 require 'rspec-puppet/example/class_example_group'
 require 'rspec-puppet/example/function_example_group'
 require 'rspec-puppet/example/host_example_group'
+require 'rspec-puppet/example/type_example_group'
+require 'rspec-puppet/example/provider_example_group'
 
 RSpec::configure do |c|
+
   def c.escaped_path(*parts)
     Regexp.compile(parts.join('[\\\/]'))
   end
@@ -24,4 +27,14 @@ RSpec::configure do |c|
   c.include RSpec::Puppet::HostExampleGroup, :type => :host, :example_group => {
     :file_path => c.escaped_path(%w[spec hosts])
   }
+
+  c.include RSpec::Puppet::TypeExampleGroup, :type => :type, :example_group => {
+    :file_path => c.escaped_path(%w[spec types])
+  }
+
+  c.include RSpec::Puppet::ProviderExampleGroup, :type => :provider, :example_group => {
+    :file_path => c.escaped_path(%w[spec providers])
+  }
+
+
 end
