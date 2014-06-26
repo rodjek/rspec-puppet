028cecd878b1f0b4f090009e4f383c8bea14a88c
Hunter Haugen <hunter@puppetlabs.com>
2014-06-26 14:42:04 -0700

    Allow tests to have strict variables
    Similar to how the future parser ability works, this will allow catalogs
    to be compiled with strict variable parsing turned on.

--- lib/rspec-puppet/support.rb
+++ lib/rspec-puppet/support.rb
@@ -124,6 +124,7 @@ module RSpec::Puppet
         [:confdir, :confdir],
         [:hiera_config, :hiera_config],
         [:parser, :parser],
+        [:strict_variables, :strict_variables],
       ].each do |a, b|
         value = self.respond_to?(b) ? self.send(b) : RSpec.configuration.send(b)
         begin
