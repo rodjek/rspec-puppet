674fab25562572c1363436247b600bb375da431d
Martin Alfke <ma@example42.com>
2016-07-05 18:18:20 +0200

    add mocks to allow cross os testing
    taken from
    https://github.com/maestrodev/maestro-puppet-example/blob/master/spec/spec_helper.rb
    
    work around https://tickets.puppetlabs.com/browse/PUP-1547
    Ticket mentions fixed in 3.6.0 but rspec testing e.g. on OSX still
    throws errors for package ensure set to latest and user with managehome
    attribute
    
    should this be configurable?

--- lib/rspec-puppet/spec_helper.rb
+++ lib/rspec-puppet/spec_helper.rb
@@ -6,4 +6,19 @@ RSpec.configure do |c|
   c.module_path = File.join(fixture_path, 'modules')
   c.manifest_dir = File.join(fixture_path, 'manifests')
   c.environmentpath = File.join(Dir.pwd, 'spec')
+  c.before(:each) do
+    # work around https://tickets.puppetlabs.com/browse/PUP-1547
+    # ensure that there's at least one provider available by emulating that any
+    if Puppet::version < '3.2'
+      # ONLY WORKING WITH PUPPET < 3.2 !!
+      require 'puppet/provider/confine/exists'
+      Puppet::Provider::Confine::Exists.any_instance.stubs(:which => '')
+    else
+      # ONLY WORKING WITH PUPPET > 3.2 !!
+      require 'puppet/confine/exists'
+      Puppet::Confine::Exists.any_instance.stubs(:which => '')
+    end
+    # avoid "Only root can execute commands as other users"
+    Puppet.features.stubs(:root? => true)
+  end
 end
