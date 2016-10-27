abe9a45f4a476ebc1c1765c83e752b4a522f92ef
Dominic Cleal <dominic@cleal.org>
2016-10-27 09:31:33 +0100

    Fix naming of test_api specs so they run
    Incorrectly named ever since they were added 7dddebd, updated to take
    into account the change of `subject` in 34220fc and rspec should/expect
    style changes.

--- spec/classes/test_api_spec.rb
+++ spec/classes/test_api_spec.rb
@@ -3,18 +3,18 @@ require 'spec_helper'
 describe 'test::bare_class' do
   describe 'rspec group' do
     it 'should have a catalogue method' do
-      catalogue.should be_a(Puppet::Resource::Catalog)
+      expect(catalogue).to be_a(Puppet::Resource::Catalog)
     end
 
     it 'subject should return a catalogue' do
-      subject.should be_a(Puppet::Resource::Catalog)
+      expect(subject.call).to be_a(Puppet::Resource::Catalog)
     end
 
     describe 'derivative group' do
       subject { catalogue.resource('Notify', 'foo') }
 
       it 'can redefine subject' do
-        subject.should be_a(Puppet::Resource)
+        expect(subject).to be_a(Puppet::Resource)
       end
     end
   end
