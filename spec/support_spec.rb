a3108275cdb0d860671f19be89563d2c10014d2d
Josh Trzebiatowski <jttrzebiato@qg.com>
2017-01-24 13:22:35 -0600

    skip {key: value} style hashes

--- spec/support_spec.rb
+++ spec/support_spec.rb
@@ -57,9 +57,6 @@ describe RSpec::Puppet::Support do
     it "should handle Hashes recursively" do
       expect(subject.str_from_value({:k1=>'v1'})).to eq('{ "k1" => "v1" }')
       expect(subject.str_from_value({'k2'=>'v2'})).to eq('{ "k2" => "v2" }')
-      if Gem::Version.new(RUBY_VERSION) >= Gem::Version.new('1.9') # Syntax not valid in Ruby 1.8.7
-        expect(subject.str_from_value({k3: 'v3'})).to eq('{ "k3" => "v3" }')
-      end
     end
   end
 end
