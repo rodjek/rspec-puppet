693c04a697ac7a2de4373f5a9b1c090943074918
Josh Trzebiatowski <jttrzebiato@qg.com>
2017-01-24 12:58:41 -0600

    Handle Ruby Symbols in nested Hashes/Arrays when testing Puppet manifest parameters

--- spec/support_spec.rb
+++ spec/support_spec.rb
@@ -32,4 +32,32 @@ describe RSpec::Puppet::Support do
       expect(subject.ref('Package','tomcat').inspect).to eq("Package['tomcat']")
     end
   end
+
+  describe '#str_from_value' do
+    it "should quote strings" do
+      expect(subject.str_from_value('a string')).to eq('"a string"')
+    end
+    it "should not quote numbers" do
+      expect(subject.str_from_value(100)).to eq('100')
+      expect(subject.str_from_value(-42)).to eq('-42')
+      expect(subject.str_from_value(3.14)).to eq('3.14')
+    end
+    it "should use literal 'default' when receiving :default" do
+      expect(subject.str_from_value(:default)).to eq('default')
+    end
+    it "should use literal 'undef' when receiving :undef" do
+      expect(subject.str_from_value(:undef)).to eq('undef')
+    end
+    it "should convert Symbols to Strings" do
+      expect(subject.str_from_value(:a_symbol)).to eq('"a_symbol"')
+    end
+    it "should handle Arrays recursively" do
+      expect(subject.str_from_value([1,2,3])).to eq('[ 1, 2, 3 ]')
+    end
+    it "should handle Hashes recursively" do
+      expect(subject.str_from_value({:k1=>'v1'})).to eq('{ "k1" => "v1" }')
+      expect(subject.str_from_value({'k2'=>'v2'})).to eq('{ "k2" => "v2" }')
+      expect(subject.str_from_value({k3: 'v3'})).to eq('{ "k3" => "v3" }')
+    end
+  end
 end
