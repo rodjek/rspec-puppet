deee1328de154183301a54741bc41dae8659c072
Jan Vansteenkiste <jan@vstone.eu>
2017-01-25 10:37:06 +0100

    Make sure all parameters are frozen before executing a function

--- lib/rspec-puppet/example/function_example_group.rb
+++ lib/rspec-puppet/example/function_example_group.rb
@@ -15,6 +15,8 @@ module RSpec::Puppet
 
       # This method is used by the `run` matcher to trigger the function execution, and provides a uniform interface across all puppet versions.
       def execute(*args)
+        # puppet 4 arguments are immutable
+        args.map(&:freeze)
         Puppet.override(@overrides, "rspec-test scope") do
           @func.call(@overrides[:global_scope], *args)
         end
