9ccbf4c17b605f3544eaa69af730793a9d3971c2
Dominic Cleal <dominic@cleal.org>
2017-02-01 08:01:33 +0000

    Correct Ruby ArgumentError class name
    Fixes #449

--- spec/applications/orch_app_spec.rb
+++ spec/applications/orch_app_spec.rb
@@ -3,16 +3,23 @@ require 'spec_helper'
 describe 'orch_app', :if => Puppet.version.to_f >= 4.3 do
   let(:node) { 'my_node' }
   let(:title) { 'my_awesome_app' }
-  let(:params) do
-    {
-      :nodes => {
-        ref('Node', node) => ref('Orch_app::Db', title),
-      },
-      :mystring => 'foobar',
-    }
+
+  context 'with params' do
+    let(:params) do
+      {
+        :nodes => {
+          ref('Node', node) => ref('Orch_app::Db', title),
+        },
+        :mystring => 'foobar',
+      }
+    end
+
+    it { should compile }
+    it { should contain_orch_app(title) }
+    it { should contain_orch_app__db(title) }
   end
 
-  it { should compile }
-  it { should contain_orch_app(title) }
-  it { should contain_orch_app__db(title) }
+  context 'missing params' do
+    it { expect { should compile }.to raise_error(ArgumentError, /provide params for an app/) }
+  end
 end
