f3d7a73003f697066e2e45ad73ef0992bac038a3
Dominic Cleal <dominic@cleal.org>
2017-02-07 11:11:44 +0100

    Disable environment override test on Puppet ~> 3.8.5
    Broken since PUP-5522 (puppetlabs/puppet@8136aff1), without a clear
    solution.

--- spec/hosts/environment_spec.rb
+++ spec/hosts/environment_spec.rb
@@ -4,7 +4,8 @@ describe 'facts.acme.com' do
   context 'without an explicit environment setting' do
     it { should contain_file('environment').with_path('rp_env') }
   end
-  context 'when specifying an explicit environment' do
+  # Broken on ~> 3.8.5 since PUP-5522
+  context 'when specifying an explicit environment', :unless => (Puppet.version >= '3.8.5' && Puppet.version.to_i < 4) do
     let(:environment) { 'test_env' }
     it { should contain_file('environment').with_path('test_env') }
     it { should contain_file('conditional_file') }
