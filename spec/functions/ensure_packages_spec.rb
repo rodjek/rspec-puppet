23b64de8804ec7c913379a103278adf6ab580afb
Tim Sharpe <tim@sharpe.id.au>
2017-03-14 11:21:28 +1100

    Support testing a pre-compiled catalogue in the create_generic matcher
    Fixes #297

---
+++ spec/functions/ensure_packages_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe 'ensure_packages' do
+  before { subject.call(['facter']) }
+
+  it 'should create the resource in the catalogue' do
+    expect(catalogue).to contain_package('facter').with_ensure('present')
+    expect(lambda { catalogue }).to contain_package('facter').with_ensure('present')
+  end
+end
