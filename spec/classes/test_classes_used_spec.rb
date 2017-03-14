b1aefbf81f14cef6230b97886ded29a35a5cdcfd
Tim Sharpe <tim@sharpe.id.au>
2017-03-14 14:13:41 +1100

    Add test case for #275

--- spec/classes/test_classes_used_spec.rb
+++ spec/classes/test_classes_used_spec.rb
@@ -12,4 +12,5 @@ describe 'test::classes_used' do
 
   it { should contain_class('test::parameterised_class').with_text('bar') }
   it { should contain_class('test::bare_class') }
+  it { should contain_class('test::parameterised_class').with_something(Proc.new { |v| v.nil? || v.empty? }) }
 end
