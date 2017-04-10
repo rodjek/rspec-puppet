3bbc0380709784fa05a0c9be78874633a313bb28
Tim Sharpe <tim@sharpe.id.au>
2017-04-10 14:16:01 +1000

    Convert fixture_path to use File.dirname

--- lib/rspec-puppet/spec_helper.rb
+++ lib/rspec-puppet/spec_helper.rb
@@ -1,6 +1,6 @@
 require 'rspec-puppet'
 
-fixture_path = File.expand_path(File.join(__FILE__, '..', 'fixtures'))
+fixture_path = File.join(File.dirname(File.expand_path(__FILE__)), 'fixtures')
 
 RSpec.configure do |c|
   c.module_path     = File.join(fixture_path, 'modules')
