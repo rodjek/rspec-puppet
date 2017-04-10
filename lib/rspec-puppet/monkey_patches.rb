086a2fd8c7450157b7050dbbcc3ee21490b25869
Tim Sharpe <tim@sharpe.id.au>
2017-04-10 12:50:42 +1000

    Another monkey patch to add support for Puppet 2.7.x

--- lib/rspec-puppet/monkey_patches.rb
+++ lib/rspec-puppet/monkey_patches.rb
@@ -26,6 +26,33 @@ module Puppet
     end
   end
 
+  # If Puppet::Node::Environment has a validet_dirs instance method (i.e.
+  # Puppet < 3.x), wrap the method to check if rspec-puppet is pretending to be
+  # running under windows. The original method uses Puppet::Util.absolute_path?
+  # (which in turn calls Puppet::Util::Platform.windows?) to validate the path
+  # to the manifests on disk during compilation, so we have to temporarily
+  # disable the pretending when running it.
+  class Node::Environment
+    if instance_methods.include?("validate_dirs")
+      old_validate_dirs = instance_method(:validate_dirs)
+
+      define_method(:validate_dirs) do |dirs|
+        pretending = false
+
+        if Puppet::Util::Platform.pretend_windows?
+          pretending = true
+          Puppet::Util::Platform.unpretend_windows
+        end
+
+        output = old_validate_dirs.bind(self).call(dirs)
+
+        Puppet::Util::Platform.pretend_windows if pretending
+
+        output
+      end
+    end
+  end
+
   module Util
     # Allow rspec-puppet to pretend to be windows.
     module Platform
