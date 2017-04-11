b54f1f55064f34562c8f1915af47b4fca7455ca4
Tim Sharpe <tim@sharpe.id.au>
2017-04-11 20:45:00 +1000

    Add positive and negative tests for #499

--- spec/classes/test_duplicate_alias_spec.rb
+++ spec/classes/test_duplicate_alias_spec.rb
@@ -2,4 +2,7 @@ require 'spec_helper'
 
 describe 'test::duplicate_alias' do
   it { should compile }
+  it { should contain_exec('foo_bar_1') }
+  it { should contain_exec('foo_bar_2') }
+  it { should_not contain_exec('/bin/echo foo bar') }
 end
