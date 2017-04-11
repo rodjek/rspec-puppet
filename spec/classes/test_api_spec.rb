3c166227f30dca60ffa3f72be9761bbb026611cd
Tim Sharpe <tim@sharpe.id.au>
2017-04-11 15:33:33 +1000

    Add spec to confirm that external resources from create_resources() are excluded from coverage

--- spec/classes/test_api_spec.rb
+++ spec/classes/test_api_spec.rb
@@ -10,15 +10,26 @@ describe 'test::bare_class' do
       expect(subject.call).to be_a(Puppet::Resource::Catalog)
     end
 
-    it 'should be included in the coverage filter' do
-      expect(RSpec::Puppet::Coverage.filters).to include('Class[Test::Bare_class]')
-    end
-
     describe 'derivative group' do
       subject { catalogue.resource('Notify', 'foo') }
 
       it 'can redefine subject' do
         expect(subject).to be_a(Puppet::Resource)
+      end
+    end
+  end
+
+  describe 'coverage' do
+    it 'class should be included in the coverage filter' do
+      expect(RSpec::Puppet::Coverage.filters).to include('Class[Test::Bare_class]')
+    end
+
+    # file and line information was only added to resources created with
+    # ensure_resource() in 4.6.0 (PUP-6530).
+    if Puppet::Util::Package.versioncmp(Puppet.version, '4.6.0') >= 0
+      it 'should not include resources from other modules created with create_resources()' do
+        expect(RSpec::Puppet::Coverage.instance.results[:resources]).to_not include('Notify[create_resources notify]')
+        expect(subject).to contain_notify('create_resources notify')
       end
     end
   end
