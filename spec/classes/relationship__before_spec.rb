rodjek/rspec-puppet

commit 9341e04fa11d1661019a2926afa31f213a3018d6

Fallback to finding resource by namevar if can't find by title

lib/rspec-puppet/matchers/create_generic.rb
@@ -214,7 +214,7 @@ module RSpec::Puppet
 
       def check_befores(catalogue, resource)
         @befores.each do |ref|
-          unless precedes?(resource, catalogue.resource(ref))
+          unless precedes?(resource, canonicalize_resource(ref))
             @errors << BeforeRelationshipError.new(resource.to_ref, ref)
           end
         end
@@ -222,7 +222,7 @@ module RSpec::Puppet
 
       def check_requires(catalogue, resource)
         @requires.each do |ref|
-          unless precedes?(catalogue.resource(ref), resource)
+          unless precedes?(canonicalize_resource(ref), resource)
             @errors << RequireRelationshipError.new(resource.to_ref, ref)
           end
         end
@@ -230,7 +230,7 @@ module RSpec::Puppet
 
       def check_notifies(catalogue, resource)
         @notifies.each do |ref|
-          unless notifies?(resource, catalogue.resource(ref))
+          unless notifies?(resource, canonicalize_resource(ref))
             @errors << NotifyRelationshipError.new(resource.to_ref, ref)
           end
         end
@@ -238,7 +238,7 @@ module RSpec::Puppet
 
       def check_subscribes(catalogue, resource)
         @subscribes.each do |ref|
-          unless notifies?(catalogue.resource(ref), resource)
+          unless notifies?(canonicalize_resource(ref), resource)
             @errors << SubscribeRelationshipError.new(resource.to_ref, ref)
           end
         end
@@ -253,7 +253,18 @@ module RSpec::Puppet
       end
 
       def canonicalize_resource(resource)
-        resource_from_ref(resource_ref(resource))
+        res = resource_from_ref(resource_ref(resource))
+        if res.nil?
+          resource = Struct.new(:type, :title).new(*@catalogue.title_key_for_ref(resource)) if resource.is_a?(String)
+          res = @catalogue.resource_keys.select { |type, name|
+            type == resource.type
+          }.map { |type, name|
+            @catalogue.resource(type, name)
+          }.first { |cat_res|
+            cat_res.builtin_type? && cat_res.uniqueness_key.first == resource.title
+          }
+        end
+        res
       end
 
       def canonicalize_resource_ref(ref)
@@ -277,9 +288,9 @@ module RSpec::Puppet
           results << relationship_refs(r, type, visited)
 
           res = canonicalize_resource(r)
-          if res.builtin_type?
+          if res && res.builtin_type?
             results << res.to_ref
-            results << "#{res.type.to_s.capitalize}[#{res.uniqueness_key}]"
+            results << "#{res.type.to_s.capitalize}[#{res.uniqueness_key.first}]"
           end
         end
 

commit a81bd5b2e33049660da91e0f5dd979d9819ffa70

Fix canonicalize_resource to only match resources with same namevar
9341e04f introduced a regression when using matchers to check that a
resource reference doesn't exist, as the matcher will find a resource of
the same type but with a different namevar/title.

The following example would fail if any `Bar` resource exists, as the
title in the `Bar[unknown]` resource reference would be ignored:

    is_expected.not_to contain_foo("foo").that_requires("Bar[unknown]")

The method passed a block to #first rather than #find in order to filter
by the namevar, but the block is unused.

lib/rspec-puppet/matchers/create_generic.rb
@@ -260,7 +260,7 @@ module RSpec::Puppet
             type == resource.type
           }.map { |type, name|
             @catalogue.resource(type, name)
-          }.first { |cat_res|
+          }.find { |cat_res|
             cat_res.builtin_type? && cat_res.uniqueness_key.first == resource.title
           }
         end

spec/classes/relationship__before_spec.rb
@@ -25,4 +25,12 @@ describe 'relationships::before' do
   it { should contain_notify('qux').that_requires(['File[/tmp/foo]']) }
   it { should contain_notify('bazz').that_comes_before(['Notify[qux]']) }
   it { should contain_notify('qux').that_requires(['Notify[bazz]']) }
+
+  it { should_not contain_notify('foo').that_comes_before('Notify[unknown]') }
+  it { should_not contain_notify('bar').that_comes_before('Notify[unknown]') }
+  it { should_not contain_notify('baz').that_comes_before('Notify[unknown]') }
+
+  it { should_not contain_notify('foo').that_requires('Notify[unknown]') }
+  it { should_not contain_notify('bar').that_requires('Notify[unknown]') }
+  it { should_not contain_notify('baz').that_requires('Notify[unknown]') }
 end