e552b3c000a77b08ef401c23377c91fe23c9e8ab
Tim Sharpe <tim@sharpe.id.au>
2017-07-02 18:59:08 +1000

    Fix Style/MethodCallwithoutArgsParetheses violations

--- spec/functions/camelCaseFunction_spec.rb
+++ spec/functions/camelCaseFunction_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe 'camelCaseFunction' do
   it { should_not be_nil }
-  it { should run.with_params().and_raise_error(Puppet::ParseError, /Requires 1 argument/) }
+  it { should run.with_params.and_raise_error(Puppet::ParseError, /Requires 1 argument/) }
   it { should run.with_params(1).and_raise_error(Puppet::ParseError, /Argument must be a string/) }
   it { should run.with_params('test').and_return('test') }
 end
