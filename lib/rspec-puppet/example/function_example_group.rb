600282cc830d6c4b70ac1cd3365431a2f4778bb9
Tim Sharpe <tim@sharpe.id.au>
2017-07-05 18:48:32 +1000

    Fix Performance/RedundantMerge violations

--- lib/rspec-puppet/example/function_example_group.rb
+++ lib/rspec-puppet/example/function_example_group.rb
@@ -197,7 +197,7 @@ module RSpec::Puppet
 
     def build_node(name, opts = {})
       node_environment = adapter.current_environment
-      opts.merge!(:environment => node_environment)
+      opts[:environment] = node_environment
       Puppet::Node.new(name, opts)
     end
   end
