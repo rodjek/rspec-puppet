afa4fdd252cd4e38b0b087bf7eeb632a9d782826
Tim Sharpe <tim@sharpe.id.au>
2018-01-15 13:21:32 +1100

    (#632) Stub out Puppet::Type::File::ProviderWindows::FeatureModule#manages_symlinks?

--- spec/classes/test_windows_spec.rb
+++ spec/classes/test_windows_spec.rb
@@ -3,6 +3,10 @@ require 'spec_helper'
 describe 'test::windows' do
   let(:facts) { {:operatingsystem => 'windows' } }
 
+  let(:symlink_path) do
+    RUBY_VERSION == '1.8.7' ? 'C:\\\\something.txt' : 'C:\\something.txt'
+  end
+
   it { should compile.with_all_deps }
-  it { should contain_file('C:\\something.txt') }
+  it { should contain_file(symlink_path) }
 end
