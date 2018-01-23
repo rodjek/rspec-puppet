2b7b89db900ec424ff23565c81ab56f78eb9f514
Gavin Williams <fatmcgav@gmail.com>
2018-01-23 07:43:22 +0000

    Fix one of the failing tests

--- spec/classes/server_facts_spec.rb
+++ spec/classes/server_facts_spec.rb
@@ -7,12 +7,17 @@ describe 'server_facts', :if => Puppet::Util::Package.versioncmp(Puppet.version,
       RSpec.configuration.trusted_server_facts = true
     end
 
+    let(:facts) {
+      {
+        :ipaddress => '192.168.1.10'
+      }
+    }
     let(:node) { 'test123.test.com' }
     it { should contain_class('server_facts') }
     it { should compile.with_all_deps }
     it { should contain_notify("servername-test123.test.com") }
-    it { should contain_notify("serverip-192.168.1.50") }
-    it { should contain_notify("serverversion-4.10.9") }
+    it { should contain_notify("serverip-192.168.1.10") }
+    it { should contain_notify("serverversion-#{Puppet.version}") }
     it { should contain_notify("environment-rp_env") }
   end
 end
