cd37eb587d453556d6fe0eac851634066cf88679
Tim Sharpe <tim@bombasticmonkey.com>
2018-08-21 12:46:53 +1000

    Unit tests for RawString

---
+++ spec/unit/raw_string_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper_unit'
+
+describe RSpec::Puppet::RawString do
+  subject(:raw_string) { described_class.new('some string') }
+
+  describe '#inspect' do
+    it 'returns an unquoted version of the string' do
+      expect(raw_string.inspect).to eq('some string')
+    end
+  end
+end
