97267a23460911d8945dbbde175b411ea79bb3a6
Tim Sharpe <tim@bombasticmonkey.com>
2019-10-07 10:53:14 +1100

    Switch raise_error specs over to old should syntax

--- spec/unit/matchers/raise_error_spec.rb
+++ spec/unit/matchers/raise_error_spec.rb
@@ -7,12 +7,12 @@ describe RSpec::Puppet::GenericMatchers::RaiseError do
   context 'with a failing target' do
     subject { lambda { fail 'catalogue load failed' } }
 
-    it { is_expected.to raise_error 'catalogue load failed' }
+    it { should raise_error 'catalogue load failed' }
   end
 
   context 'with a passing target' do
     subject { lambda { } }
 
-    it { is_expected.not_to raise_error }
+    it { should_not raise_error }
   end
 end
