65c4172e8e5e790d9eb5451662aef256b4b167a7
Sean Millichamp <sean.millichamp@global.ntt>
2022-01-11 13:42:50 -0500

    Handle nil autorequire results
    When encountering results containing nil from an autorequire-family
    call, Puppet will skip over them without error but rspec-puppet
    results in an error. This aligns rspec-puppet with Puppet's
    behavior.
    
    Also, rename the autorelationships spec test so that it runs
    properly as part of a 'rake spec' invocation.

--- spec/fixtures/modules/relationships/lib/puppet/type/type_with_all_auto.rb
+++ spec/fixtures/modules/relationships/lib/puppet/type/type_with_all_auto.rb
@@ -1,8 +1,8 @@
 Puppet::Type.newtype(:type_with_all_auto) do
   ensurable
   newparam(:name, :namevar => true)
-  autobefore(:file) { [File.join(self[:name], 'before')] }
-  autonotify(:file) { [File.join(self[:name], 'notify')] }
-  autorequire(:file) { [File.join(self[:name], 'require')] }
-  autosubscribe(:file) { [File.join(self[:name], 'subscribe')] }
+  autobefore(:file) { [File.join(self[:name], 'before'), nil] }
+  autonotify(:file) { [File.join(self[:name], 'notify'), nil] }
+  autorequire(:file) { [File.join(self[:name], 'require'), nil] }
+  autosubscribe(:file) { [File.join(self[:name], 'subscribe'), nil] }
 end
