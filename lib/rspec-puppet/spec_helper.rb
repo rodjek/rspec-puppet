0591c352314c88b28adb589a5047fdd7cdc51cbe
jordanbreen28 <jordan.breen@perforce.com>
2023-10-02 15:11:25 +0100

    (maint) - Update tests

--- lib/rspec-puppet/spec_helper.rb
+++ lib/rspec-puppet/spec_helper.rb
@@ -4,7 +4,6 @@ require 'rspec-puppet'
 
 RSpec.configure do |c|
   c.module_path     = File.join(fixture_path, 'modules')
-  c.manifest_dir    = File.join(fixture_path, 'manifests')
   c.manifest        = File.join(fixture_path, 'manifests', 'site.pp')
   c.environmentpath = File.join(Dir.pwd, 'spec')
 end
--- spec/classes/server_facts_spec.rb
+++ spec/classes/server_facts_spec.rb
@@ -10,11 +10,10 @@ describe 'server_facts' do
   end
   let(:node) { 'test123.test.com' }
 
-    it { is_expected.to contain_class('server_facts') }
-    it { is_expected.to compile.with_all_deps }
-    it { is_expected.to contain_notify('servername-test123.test.com') }
-    it { is_expected.to contain_notify('serverip-192.168.1.10') }
-    it { is_expected.to contain_notify("serverversion-#{Puppet.version}") }
-    it { is_expected.to contain_notify('environment-rp_env') }
-  end
+  it { is_expected.to contain_class('server_facts') }
+  it { is_expected.to compile.with_all_deps }
+  it { is_expected.to contain_notify('servername-test123.test.com') }
+  it { is_expected.to contain_notify('serverip-192.168.1.10') }
+  it { is_expected.to contain_notify("serverversion-#{Puppet.version}") }
+  it { is_expected.to contain_notify('environment-rp_env') }
 end
